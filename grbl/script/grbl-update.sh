4522bc2dbb5529a4a7785f48d2f019219bb40535
frog <david you>
2019-03-22 17:48:36 +0900

    backup

--- grbl/script/grbl-update.sh
+++ grbl/script/grbl-update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "grbl update to uno"
 
-test -e ../firmware/grbl_v1.1f.20170801.hex || wget https://github.com/gnea/grbl/releases/download/v1.1f.20170801/grbl_v1.1f.20170801.hex -o ../firmware/grbl_v1.1f.20170801.hex
+test -e ../firmware/grbl_v1.1f.20170801.hex || wget https://github.com/gnea/grbl/releases/download/v1.1f.20170801/grbl_v1.1f.20170801.hex -O ../firmware/grbl_v1.1f.20170801.hex
 test -e /usr/bin/avrdude || sudo apt-get install -y avrdude
-sudo avrdude -v -patmega328p -Uflash:w:../firmware/grbl_v1.1f.20170801.hex:i -carduino -b 57600 -P /dev/ttyUSB1
+# sudo avrdude -v -patmega328p -Uflash:w:../firmware/grbl_v1.1f.20170801.hex:i -carduino -b 57600 -P /dev/ttyUSB1
+sudo avrdude -v -patmega328p -Uflash:w:../firmware/grbl_v1.1f.20170801.hex:i -carduino -b 115200 -P /dev/ttyUSB1
